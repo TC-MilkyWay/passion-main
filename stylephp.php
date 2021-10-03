50770aa4b6771c782931d959b4341b843d3191e6
Yohann Gille <90717760+TC-MilkyWay@users.noreply.github.com>
2021-10-03 23:19:26 +0200

    Add files via upload

---
+++ stylephp.php
@@ -0,0 +1,11 @@
+<nav class="menu">
+                <ul>
+                    <li><a href="capitales.html">Les Capitales les plus visités</a></li>
+                    <li><a href="asie.html">L'Asie</a></li>
+                    <li><a href="ameriquen.html">L'Amérique du nord</a></li>
+                    <li><a href="europe.html">L'Europe</a></li>
+                    <li><a href="afrique.html">L'Afrique</a></li>
+                    <li><a href="mesenvies.html">Mes Envies</a></li>
+                    <li><a href="mesvoyages.html">Mes Voyages</a></li>  
+                 </ul>
+            </nav>
